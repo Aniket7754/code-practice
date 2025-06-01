34cfe085b6f15a18cb0363e3c15f3a8634469a88
Aniket7754 <aniketkesharwani470@gmail.com>
2025-06-01 16:43:49 +0530

    Number

--- newpro.js
+++ newpro.js
@@ -2,9 +2,9 @@ const newpromise = new Promise((resolve , reject) => {
     setTimeout(() => resolve(5) , 1000)
 })
 
-newpromise.then(result => {
-    console.log("result" , result*2)
-})
+// newpromise.then(result => {
+//     console.log("result" , result*2)
+// })
 
 Promise.resolve(10)
   .then(x => x + 5)
