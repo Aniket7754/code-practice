6aaabf188007064f5d819b369fd7afbc5e455fd1
ADARSH KESHARI <aniketkumar@Anikets-MacBook-Air.local>
2026-01-18 11:07:21 +0530

    zeros

--- zeros.js
+++ zeros.js
@@ -1,2 +1,19 @@
 // move the all zeros at the end 
 
+function movezeros(arr){
+    let pos = 0
+    for (let i =0 ; i < arr.length ; i++){
+        if(arr[i] !== 0){
+            // c÷onsole.log(arr[i])
+            arr[pos] = arr[i] 
+            pos++
+        }
+    }
+    console.log(pos)
+    while(pos < arr.length){
+        arr[pos] = 0
+        pos++
+    }
+    return arr
+}
+console.log(movezeros([0 ,1 ,1 ,0 ,2 ,0 ,0 ,0]))
